0472eba59aa581ac306cad753384cc89c6873ab2
Shany Abady <s.abady@gmail.com>
2025-02-06 11:46:55 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-podman build --jobs=4 --platform="linux/arm64,linux/amd64" --progress=plain --layers=true --compress --format=docker --manifest "harbor.belong.life/devops/wiki-on-a-llama:${version}_${BUILD_NUMBER}" -f ./Dockerfile 
-podman manifest push --all harbor.belong.life/devops/wiki-on-a-llama:${version}_${BUILD_NUMBER} docker://harbor.belong.life/devops/wiki-on-a-llama:${version}_${BUILD_NUMBER}
+podman build --jobs=4 --platform="linux/arm64,linux/amd64" --progress=plain --layers=true --compress --format=docker --manifest "harbor.example.com/devops/wiki-on-a-llama:${version}_${BUILD_NUMBER}" -f ./Dockerfile 
+podman manifest push --all harbor.example.com/devops/wiki-on-a-llama:${version}_${BUILD_NUMBER} docker://harbor.example.com/devops/wiki-on-a-llama:${version}_${BUILD_NUMBER}
 
